d002195006caf69124ba0e9a91d5d7d91494b427
phangjunyu <phangjunyu@gmail.com>
2018-10-29 15:39:57 +0800

    lolool

--- assignment_solution/noun_phrase_summarizer.py
+++ assignment_solution/noun_phrase_summarizer.py
@@ -1,13 +1,18 @@
 from dbmgr.models import AmazonReview
-from nltk import RegexpParser, word_tokenize, sent_tokenize, pos_tag_sents
-from nltk.corpus import conll2000
+from nltk import RegexpParser, word_tokenize, pos_tag
+from nltk.stem import WordNetLemmatizer
+# from nltk.corpus import conll2000
 from collections import Counter
 
 def sandbox():
 	c = Counter()
-	teststr = "this is a very hairy dog with teeth that are very large. This is sentence number two. How about question marks? And a sentence with fullstops without spaces.Like this sentence."
-	extract_np(c, teststr)
-	print(c.most_common(10))
+	data = AmazonReview.objects.filter(asin='B005SUHPO6')[4:5]
+	for index, d in enumerate(data):
+		extract_np(c, d.reviewText)
+		if index%100 == 0:
+			print("\rfinished %d iterations" %index, end="") 
+	
+	print("\n {}".format(c.most_common(20)))
 
 def extract_top_20_from_reviews():
 	c = Counter()
@@ -33,39 +38,48 @@ def repr_np_from_3_popular_products():
 
 """
 a.
-1. clean the words
-2. tf-idf
-3. stem and count
+1. tf-idf
+2. remove repeated words
 
 b. try training a classifier
+"""
+
+"""
+for part 1: we want to have a slacker rule - NBAR: {<NN.*|JJ.*>*<NN.*>} NP: {<NBAR>} {<NBAR><IN><NBAR>}
+for part 2: we want adjectives cause they are more representative
 """
 
 def extract_np(c, data):
 	#refine the grammar?
 	grammar = r"""
-        NP: {<PDT>?<DT>?<JJ|JJR|JJS>+<NN|NNS|NNP|NNPS>+}
+        NP: {<PDT|DT>+<JJ|JJR|JJS>+<NN|NNS|NNP|NNPS>+}
             # {<NN|NNS|NNP|NNPS>+}
     """
     
 	cp = RegexpParser(grammar)
 
-	text = [word_tokenize(t) for t in sent_tokenize(data)]
-	parsed_sentenced = pos_tag_sents(text)
+	text = word_tokenize(data)
+	sentence = pos_tag(text)
 	result = []
-	for sentence in parsed_sentenced:
-		parsed_sentence = cp.parse(sentence)
-		# # Clearer visuals for debugging
-		print(parsed_sentence)
-		# parsed_sentence.draw()
-		for np in clean_np(parsed_sentence):
-			result.append(np)
+	parsed_sentence = cp.parse(sentence)
+	# # Clearer visuals for debugging
+	# print(parsed_sentence)
+	# parsed_sentence.draw()
+	for np in clean_np(parsed_sentence):
+		result.append(np)
 	
+
+	lemmatizer = WordNetLemmatizer()
+	# print("\nreview text is: {}".format(data))
+	# print("\nbefore lowercase and lemmatization: {}".format(result))
+	result = [str.lower(r) for r in result]
+	result = [lemmatizer.lemmatize(r) for r in result]
 	c.update(result)
 
 def clean_np(parsed_sentence):
-    for subtree in parsed_sentence.subtrees():
-        if subtree.label() == 'NP':
-        	yield ' '.join(word for word, tag in subtree.leaves())
+	for subtree in parsed_sentence.subtrees():
+		if subtree.label() == 'NP':
+			yield ' '.join(word for word,tag in subtree.leaves() if not (tag == 'DT' and str.lower(word) in ['the', 'a', 'an', 'these', 'that', 'this', 'those']))
 
 
 
